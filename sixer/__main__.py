5690180f85c993c7e7c1f2fbe0ca07bc2d05bb14
Taylor Jackle Spriggs <taylor@sigopt.com>
2018-08-16 13:50:12 -0700

    sixer main

--- sixer/__main__.py
+++ sixer/__main__.py
@@ -1,15 +1,34 @@
 import argparse
+import os
 import sys
 
 from sixer import check_file
 
-def main(args):
-    source_names = args.file
-    if any([check_file(source_name) for source_name in source_names]):
-        return 1
-    return 0
+def valid_file(filename):
+    return filename.endswith('.py') and os.path.isfile(filename)
 
-if __name__ == '__main__':
+def all_paths(path):
+    for dirpath, _, filenames in os.walk(path):
+        for filename in filenames:
+            full_filename = os.path.join(dirpath, filename)
+            if valid_file(full_filename):
+                yield full_filename
+
+def all_files(paths):
+    for path in paths:
+        for filename in all_paths(path):
+            yield filename
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('file', nargs='+', help="Files to check")
-    sys.exit(main(parser.parse_args()))
+    args = parser.parse_args()
+    source_names = all_files(args.file)
+    if any([check_file(source_name) for source_name in source_names]):
+        exit_code = 1
+    else:
+        exit_code = 0
+    return exit_code
+
+if __name__ == '__main__':
+    sys.exit(main())
