f5964006aa0c33d7ead01d270679c0280e61d0b0
Taylor Jackle Spriggs <taylor@sigopt.com>
2018-08-16 13:50:33 -0700

    setuptools

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+MAJOR = 0
+MINOR = 0
+MICRO = 1
+
+VERSION = '{}.{}.{}'.format(MAJOR, MINOR, MICRO)
+
+
+setup(
+  name='sixer',
+  version=VERSION,
+  description='Python 2/3 compatibility linter',
+  author='Taylor Jackle Spriggs',
+  author_email='fndasltn@gmail.com',
+  url='https://github.com/taylorjacklespriggs/sixer',
+  packages=find_packages(),
+  scripts=[
+    'bin/sixer',
+  ]
+)
