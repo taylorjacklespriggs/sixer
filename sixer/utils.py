5762de6df54828c77c78bad16037b9dc1f5d78f6
Taylor Jackle Spriggs <taylor@sigopt.com>
2018-08-16 14:09:27 -0700

    catch syntax error

--- sixer/utils.py
+++ sixer/utils.py
@@ -5,16 +5,28 @@ import sys
 
 from sixer.rules import prepare_all_rules
 
-def check_file(source_name, out_file=sys.stderr):
+def print_problem(source_name, lineno, col_offset, message):
+    print("{}:{}:{}: {}".format(source_name, lineno, col_offset, message))
+
+
+def check_file(source_name):
     with open(source_name, 'r') as source_file:
         raw_source = source_file.read()
     all_lint_node_rules = list(prepare_all_rules(raw_source))
-    tree = ast.parse(raw_source, source_name)
+    try:
+        tree = ast.parse(raw_source, source_name)
+    except SyntaxError as se:
+        print_problem(source_name, se.lineno, se.offset, "syntax error")
+        return True
     for node in ast.walk(tree):
         for rule in all_lint_node_rules:
             rule.check_node(node)
-    all_node_problems = [problem for rule in all_lint_node_rules for problem in rule.get_problems()]
+    all_node_problems = [
+        problem
+        for rule in all_lint_node_rules
+        for problem in rule.get_problems()
+    ]
     all_node_problems.sort(key=lambda p: (p[1].lineno, p[1].col_offset))
     for message, node in all_node_problems:
-        print("{}:{}:{}: {}".format(source_name, node.lineno, node.col_offset, message))
+        print_problem(source_name, node.lineno, node.col_offset, message)
     return bool(all_node_problems)
