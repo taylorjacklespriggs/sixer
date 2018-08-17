f0c682268915396b1b1945fb79634a6f72ac9ea6
Taylor Jackle Spriggs <taylor@sigopt.com>
2018-08-16 18:11:07 -0700

    keyword only rule

---
+++ sixer/rules/keyword_rule.py
@@ -0,0 +1,14 @@
+import ast
+import six
+
+from sixer.rules.lint_node_rule import LintNodeRule
+
+
+class KeywordOnlyRule(LintNodeRule):
+    is_valid = six.PY3
+
+    def _get_problem_message(self, node):
+        return "keywords only arguments not allowed"
+
+    def _is_problem_node(self, node):
+        return isinstance(node, ast.arguments) and node.kwonlyargs
